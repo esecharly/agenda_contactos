21a5eed5bfcd7fd92f350a531e0c1fa07fa87cb0
esecharly <62493210+esecharly@users.noreply.github.com>
2021-05-06 21:16:48 -0500

    create repository agenda_contactos

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Agenda de contactos web</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<?php require_once "dependencias.php"; ?>
+</head>
+<body>
+	<div class="container">
+		<?php require_once "menu.php"; ?>
+
+		<div class="jumbotron">
+			<h1 class="display-4">Agenda de contactos con PHP y MySQL</h1>
+
+			<div class="row">
+				<div class="col-sm-6">
+					<div class="card">
+						<div class="card-body">
+							<img src="public/img/af65bff950bcbb49d1ab182e9fe2a193.jpg" class="img-fluid" alt="Responsive image">
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6">
+					<div class="card">
+						<div class="card-body">
+							<table class="table table-hover">
+								<tr>
+									<th>
+										Proyecto creado en 2021
+									</th>
+								</tr>
+								<tr>
+									<th>
+										By esecharly
+									</th>
+								</tr>
+							</table>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<hr class="my-4">
+			<p class="text-right">
+				Agenda de contactos con php y mysql
+			</p>
+			
+		</div>
+
+	</div>
+
+	
+</body>
+</html>
